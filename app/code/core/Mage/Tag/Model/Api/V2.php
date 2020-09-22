347172f80983db6f2583c436f5660c0c58ab008c
Chris Greben <greben99@hotmail.com>
2020-09-22 15:49:10 +1000

    [tag] Child needs to match parent declaration

--- app/code/core/Mage/Tag/Model/Api/V2.php
+++ app/code/core/Mage/Tag/Model/Api/V2.php
@@ -31,8 +31,7 @@
  * @package    Mage_Tag
  * @author     Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
-{
+class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api {
     /**
      * Retrieve list of tags for specified product as array of objects
      *
@@ -40,8 +39,7 @@ class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
      * @param string|int $store
      * @return array
      */
-    public function items($productId, $store)
-    {
+    public function items($productId, $store = null) {
         $result = parent::items($productId, $store);
         foreach ($result as $key => $tag) {
             $result[$key] = Mage::helper('api')->wsiArrayPacker($tag);
@@ -56,11 +54,10 @@ class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
      * @param array $data
      * @return array
      */
-    public function add($data)
-    {
-        $result = array();
+    public function add($data) {
+        $result = [];
         foreach (parent::add($data) as $key => $value) {
-            $result[] = array('key' => $key, 'value' => $value);
+            $result[] = ['key' => $key, 'value' => $value];
         }
 
         return $result;
@@ -73,12 +70,11 @@ class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
      * @param string|int $store
      * @return object
      */
-    public function info($tagId, $store)
-    {
+    public function info($tagId, $store) {
         $result = parent::info($tagId, $store);
         $result = Mage::helper('api')->wsiArrayPacker($result);
         foreach ($result->products as $key => $value) {
-            $result->products[$key] = array('key' => $key, 'value' => $value);
+            $result->products[$key] = ['key' => $key, 'value' => $value];
         }
         return $result;
     }
@@ -89,8 +85,7 @@ class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
      * @param object $data
      * @return array
      */
-    protected function _prepareDataForAdd($data)
-    {
+    protected function _prepareDataForAdd($data) {
         Mage::helper('api')->toArray($data);
         return parent::_prepareDataForAdd($data);
     }
@@ -101,8 +96,7 @@ class Mage_Tag_Model_Api_V2 extends Mage_Tag_Model_Api
      * @param object $data
      * @return array
      */
-    protected function _prepareDataForUpdate($data)
-    {
+    protected function _prepareDataForUpdate($data) {
         Mage::helper('api')->toArray($data);
         return parent::_prepareDataForUpdate($data);
     }
