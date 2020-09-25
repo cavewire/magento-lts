181f151aab7177283d21c1f501bb7dae2790d02a
Chris Greben <greben99@hotmail.com>
2020-09-25 11:20:13 +1000

    [core] Remove REMOTE check

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -56,7 +56,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
      */
     public function start($sessionName = null)
     {
-        if (isset($_SESSION) && !$this->getSkipEmptySessionCheck() && !array_key_exists('REMOTE_ADDR', $_SERVER)) {
+        if (isset($_SESSION) && !$this->getSkipEmptySessionCheck()) {
             return $this;
         }
 
