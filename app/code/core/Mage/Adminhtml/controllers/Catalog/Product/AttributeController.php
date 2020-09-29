a80de6b5de716709637169aae86fd5b121f302f5
Chris Greben <greben99@hotmail.com>
2020-09-29 10:56:02 +1000

    [adminhtml] Removed Attribute Validation

--- app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php
+++ app/code/core/Mage/Adminhtml/controllers/Catalog/Product/AttributeController.php
@@ -212,11 +212,11 @@ class Mage_Adminhtml_Catalog_Product_AttributeController extends Mage_Adminhtml_
             if (isset($data['attribute_code'])) {
                 $validatorAttrCode = new Zend_Validate_Regex(array('pattern' => '/^(?!event$)[a-z][a-z_0-9]{1,254}$/'));
                 if (!$validatorAttrCode->isValid($data['attribute_code'])) {
-                    $session->addError(
-                        Mage::helper('catalog')->__('Attribute code is invalid. Please use only letters (a-z), numbers (0-9) or underscore(_) in this field, first character should be a letter. Do not use "event" for an attribute code.')
-                    );
-                    $this->_redirect('*/*/edit', array('attribute_id' => $id, '_current' => true));
-                    return;
+                    // $session->addError(
+                    //     Mage::helper('catalog')->__('Attribute code is invalid. Please use only letters (a-z), numbers (0-9) or underscore(_) in this field, first character should be a letter. Do not use "event" for an attribute code.')
+                    // );
+                    // $this->_redirect('*/*/edit', array('attribute_id' => $id, '_current' => true));
+                    // return;
                 }
             }
 
