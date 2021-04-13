e9dd872d1c268d60d33bcf919998c3e31756d9cd
Chris Greben <greben99@hotmail.com>
2021-04-13 15:38:45 +1000

    [form] Allow PDF view in image preview

--- lib/Varien/Data/Form/Element/Image.php
+++ lib/Varien/Data/Form/Element/Image.php
@@ -40,8 +40,7 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @param array $data
      */
-    public function __construct($data)
-    {
+    public function __construct($data) {
         parent::__construct($data);
         $this->setType('file');
     }
@@ -51,22 +50,37 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @return string
      */
-    public function getElementHtml()
-    {
+    public function getElementHtml() {
         $html = '';
 
-        if ((string)$this->getValue()) {
+        if ((string) $this->getValue()) {
             $url = $this->_getUrl();
 
-            if( !preg_match("/^http\:\/\/|https\:\/\//", $url) ) {
+            if (!preg_match("/^http\:\/\/|https\:\/\//", $url)) {
                 $url = Mage::getBaseUrl('media') . $url;
             }
 
-            $html = '<a href="' . $url . '"'
+            $ext = substr($url, -3);
+
+            if ($ext === 'pdf') {
+                $width  = '300px';
+                $height = '';
+                
+                if ($this->getData('width')) {
+                    $width  = $this->getData('width');
+                }
+                if ($this->getData('height')) {
+                    $height  = $this->getData('height');
+                }
+
+                $html   = "<embed class='small-image-preview' id=\"" . $this->getHtmlId() . '" src="' . $url . '#nameddest=self&page=1&view=FitH,0&zoom=80,0,0" type="application/pdf" height="' . $height . '" width="' . $width . '" />';
+            } else {
+                $html = '<a href="' . $url . '"'
                 . ' onclick="imagePreview(\'' . $this->getHtmlId() . '_image\'); return false;">'
                 . '<img src="' . $url . '" id="' . $this->getHtmlId() . '_image" title="' . $this->getValue() . '"'
                 . ' alt="' . $this->getValue() . '" height="22" width="22" class="small-image-preview v-middle" />'
                 . '</a> ';
+            }
         }
         $this->setClass('input-file');
         $html .= parent::getElementHtml();
@@ -80,15 +94,14 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @return string
      */
-    protected function _getDeleteCheckbox()
-    {
+    protected function _getDeleteCheckbox() {
         $html = '';
         if ($this->getValue()) {
             $label = Mage::helper('core')->__('Delete Image');
             $html .= '<span class="delete-image">';
             $html .= '<input type="checkbox"'
                 . ' name="' . parent::getName() . '[delete]" value="1" class="checkbox"'
-                . ' id="' . $this->getHtmlId() . '_delete"' . ($this->getDisabled() ? ' disabled="disabled"': '')
+                . ' id="' . $this->getHtmlId() . '_delete"' . ($this->getDisabled() ? ' disabled="disabled"' : '')
                 . '/>';
             $html .= '<label for="' . $this->getHtmlId() . '_delete"'
                 . ($this->getDisabled() ? ' class="disabled"' : '') . '> ' . $label . '</label>';
@@ -104,8 +117,7 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @return string
      */
-    protected function _getHiddenInput()
-    {
+    protected function _getHiddenInput() {
         return '<input type="hidden" name="' . parent::getName() . '[value]" value="' . $this->getValue() . '" />';
     }
 
@@ -114,8 +126,7 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @return string
      */
-    protected function _getUrl()
-    {
+    protected function _getUrl() {
         return $this->getValue();
     }
 
@@ -124,8 +135,7 @@ class Varien_Data_Form_Element_Image extends Varien_Data_Form_Element_Abstract
      *
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return  $this->getData('name');
     }
 }
