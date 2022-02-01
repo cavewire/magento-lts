1611bf7f29b5d757d70a44e30e0fd7810e55c80d
Chris Greben <greben99@hotmail.com>
2022-02-01 13:41:14 +1000

    [customer] Removed cache=false

--- app/code/core/Mage/Customer/controllers/AccountController.php
+++ app/code/core/Mage/Customer/controllers/AccountController.php
@@ -225,7 +225,7 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
                 $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
             }
         }
-        $this->_redirectUrl($session->getBeforeAuthUrl(true).'?cache=false');
+        $this->_redirectUrl($session->getBeforeAuthUrl(true));
     }
 
     /**
