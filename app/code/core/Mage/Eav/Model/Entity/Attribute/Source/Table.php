1476795ffe0a9bba658863a1b1135b211d3de416
Chris Greben <greben99@hotmail.com>
2022-02-08 16:55:17 +1000

    [eav] PHP8.1

--- app/code/core/Mage/Eav/Model/Entity/Attribute/Source/Table.php
+++ app/code/core/Mage/Eav/Model/Entity/Attribute/Source/Table.php
@@ -90,7 +90,7 @@ class Mage_Eav_Model_Entity_Attribute_Source_Table extends Mage_Eav_Model_Entity
         }
         
         $isMultiple = false;
-        if (strpos($value, ',')) {
+        if (strpos($value ?? '', ',')) {
             $isMultiple = true;
             $value = explode(',', $value);
         }
