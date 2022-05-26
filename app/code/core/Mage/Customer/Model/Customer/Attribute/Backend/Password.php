6aef71ab06829f193ef097764f75ee8c42a5ee61
Chris Greben <chris.greben@gmail.com>
2022-05-26 17:37:48 +1000

    [customer] Log error trim null obj

--- app/code/core/Mage/Customer/Model/Customer/Attribute/Backend/Password.php
+++ app/code/core/Mage/Customer/Model/Customer/Attribute/Backend/Password.php
@@ -43,7 +43,7 @@ class Mage_Customer_Model_Customer_Attribute_Backend_Password extends Mage_Eav_M
      */
     public function beforeSave($object)
     {
-        $password = trim($object->getPassword());
+        $password = is_null($object->getPassword()) ? '' : trim($object->getPassword());
         $len = Mage::helper('core/string')->strlen($password);
         if ($len) {
             $minPasswordLength = Mage::getModel('customer/customer')->getMinPasswordLength();
