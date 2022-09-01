49c95bf84388cb67ae9a53ddab1e4e01f7768784
Chris Greben <chris.greben@gmail.com>
2022-09-01 16:05:38 +1000

    [core] Serialize Error Fix

--- app/code/core/Mage/Core/Helper/UnserializeArray.php
+++ app/code/core/Mage/Core/Helper/UnserializeArray.php
@@ -34,13 +34,51 @@
 class Mage_Core_Helper_UnserializeArray
 {
     /**
+     * CG Added to prevent error. Check if string is serlialized before calling
+     * Error during unserialization in /lib/Unserialize/Parser.php:60
+     */
+    function isSerialized($data) {
+        // if it isn't a string, it isn't serialized
+        if ( ! is_string( $data ) )
+            return false;
+        $data = trim( $data );
+        if ( 'N;' == $data )
+            return true;
+        $length = strlen( $data );
+        if ( $length < 4 )
+            return false;
+        if ( ':' !== $data[1] )
+            return false;
+        $lastc = $data[$length-1];
+        if ( ';' !== $lastc && '}' !== $lastc )
+            return false;
+        $token = $data[0];
+        switch ( $token ) {
+            case 's' :
+                if ( '"' !== $data[$length-2] )
+                    return false;
+            case 'a' :
+            case 'O' :
+                return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+            case 'b' :
+            case 'i' :
+            case 'd' :
+                return (bool) preg_match( "/^{$token}:[0-9.E-]+;\$/", $data );
+        }
+        return false;
+    }
+
+    /**
      * @param string $str
      * @return array
      * @throws Exception
      */
     public function unserialize($str)
     {
-        $parser = new Unserialize_Parser();
-        return $parser->unserialize($str);
+        if($this->isSerialized($str)) {
+            $parser = new Unserialize_Parser();
+            return $parser->unserialize($str);
+        }
+        return $str;
     }
 }
